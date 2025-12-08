917528688ce5be6cf4ba30d43d3d86e793aa9cc9
ornella28 <angelnibaruta88@gmail.com>
2025-12-08 22:39:29 +0100

    exercise 4, 5

--- src/main/java/se/lexicon/ArraysExercises.java
+++ src/main/java/se/lexicon/ArraysExercises.java
@@ -6,9 +6,11 @@ public class ArraysExercises {
 
     void main(){
 
-        //ex1();
+        ex1();
         indexOf();
         ex3();
+        ex4();
+        ex5();
 
 
 
@@ -24,6 +26,7 @@ public class ArraysExercises {
         System.out.println(numbers[2]);
 
 
+
     }
 //exercise2
     static void indexOf(){
@@ -34,6 +37,7 @@ public class ArraysExercises {
 
             System.out.println("The index position of the number [" + numbers[i] + "] =" + i);
 
+
         }
 
     }
@@ -43,11 +47,39 @@ public class ArraysExercises {
         Arrays.sort(countries);
         for (String country: countries){// the loop is needed to have some output
             System.out.println(country);
+
         }
 
     }
 
     static void ex4(){
+        int [] array1 = {1, 15, 20};
+        int[] array2 = Arrays.copyOf(array1, 3);
+        for (int number: array2){
+            System.out.println(number);
+
+        }
+
+
+    }
+
+    static void ex5(){
+        String [][] countries = new String[2][2];
+
+        countries [0][0] = "France";
+        countries[0][1] = "Paris";
+
+        countries[1][0] = "Sweden";
+        countries[1][1] = "Stockholm";
+
+        for (int i=0; i< countries.length; i++){
+
+                System.out.println(countries[i][0] + " " + countries[i][1]);
+
+        }
+
+
+
 
     }
 
